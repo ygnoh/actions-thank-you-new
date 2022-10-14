38dc1788e386c03135653f912eb2d79e6822a1db
yonggoo-noh <yonggoo.noh@navercorp.com>
2022-10-14 13:41:17 +0900

    Create a comment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Thank you for your contribution!");
+const github = require("@actions/github");
+const core = require("@actions/core");
+
+async function run() {
+    const {GITHUB_REPOSITORY, GITHUB_REF} = process.env;
+    const prNum = GITHUB_REF.match(/^refs\/pull\/(.+)\/merge$/)[1];
+    const [owner, repo] = GITHUB_REPOSITORY.split("/");
+    const token = core.getInput("token");
+    const octokit = github.getOctokit(token)
+
+    await octokit.rest.issues.createComment({
+        owner,
+        repo,
+        issue_number: prNum,
+        body: "Thank you for your contribution!"
+    });
+}
+
+run();
